e879decf0b5692e2e1e8159bac0705ec87eeee29
Danjuma Ibrahim <ufidfarooq@gmail.com>
2025-12-31 22:57:06 +0000

    Add comments to seek-destroy.js

--- data-structures/seek-destroy.js
+++ data-structures/seek-destroy.js
@@ -1,5 +1,7 @@
 
+// Remove all elements from the initial array that are of the same value as these arguments.
 function destroyer (arr, ...more) {
+  // Use filter to keep elements that are NOT included in the 'more' arguments
   return arr.filter(elem => !more.includes(elem));
 }
 
